a53e959ecfb4706a390633cb0becb21ff648e878
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-30 15:06:06 +0300

    Add validation and save correct form data

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -12,7 +12,35 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  /*
+    GlobalKey<FormState> создаёт ключ, с помощью которого можно получить состояние формы 
+    (FormState) из любого места в этом виджете.
+    Благодаря этому можно вызывать методы формы (validate(), save(), reset()) 
+    вне виджета Form (например, в методе _submit).
+  */
+
+  final _form = GlobalKey<FormState>();
+
   var _isLogin = true;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    /*
+      currentState возвращает объект FormState, который содержит состояние всех полей внутри формы.
+      Метод validate() проходит по всем FormField (в нашем случае TextFormField) внутри этой формы и 
+      вызывает их validator.
+      Если все validator возвращают null, значит данные валидны → validate() возвращает true.
+      Если хотя бы один validator возвращает строку (сообщение об ошибке), validate() вернёт false и 
+      покажет ошибки под соответствующими полями.
+    */
+    final isValid = _form.currentState!.validate();
+    if (isValid) {
+      _form.currentState!.save();
+      print(_enteredEmail);
+      print(_enteredPassword);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +68,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(16),
                     child: Form(
+                      key: _form,
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -50,6 +79,18 @@ class _AuthScreenState extends State<AuthScreen> {
                             keyboardType: TextInputType.emailAddress,
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
+                                return 'Please enter a valid email address.';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              // onSaved вызывается только при вызове _form.currentState!.save().
+                              _enteredEmail = value!;
+                            },
                           ),
 
                           TextFormField(
@@ -57,12 +98,22 @@ class _AuthScreenState extends State<AuthScreen> {
                               labelText: 'Password',
                             ),
                             obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.trim().length < 6) {
+                                return 'Password must be at least 6 characters long.';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              // onSaved вызывается только при вызове _form.currentState!.save().
+                              _enteredPassword = value!;
+                            },
                           ),
 
                           const SizedBox(height: 12),
 
                           ElevatedButton(
-                            onPressed: () {},
+                            onPressed: _submit,
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Theme.of(
                                 context,
