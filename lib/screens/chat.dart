76f14939a964a749c9510b0229ec6cd141a8a623
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-31 16:38:39 +0300

    Add authkey for iOS PN

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -2,9 +2,32 @@ import 'package:chatapp/widgets/chat_messages.dart';
 import 'package:chatapp/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    //  final token = fcm.getToken();
+    fcm.subscribeToTopic('chat');
+
+    
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +44,12 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(children: const [
-        Expanded(child: ChatMessages()), 
-        NewMessage()
-        ]),
+      body: Column(
+        children: const [
+          Expanded(child: ChatMessages()),
+          NewMessage(),
+        ],
+      ),
     );
   }
 }
